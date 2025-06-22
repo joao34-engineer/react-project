0d36b16617eb64d51aeec12edcc4c33a5b4bc1d6
Your Name <your_email@example.com>
2025-06-22 20:00:43 -0300

    commit

---
+++ backend/server.js
@@ -0,0 +1,13 @@
+import express from "express"
+
+const app = express()
+
+app.get("/api/notes", (req, res) => {
+    res.status(200).send("you got 10 notes ");
+});
+
+app.listen(5001, ()  => {
+    console.log("server started on PORT: 5001");
+
+});
+
