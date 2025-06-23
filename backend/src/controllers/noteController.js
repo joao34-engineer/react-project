18ff7c624202c3db0466b89e9b40c7ce20e797cf
Your Name <your_email@example.com>
2025-06-22 22:35:18 -0300

    start with the database

---
+++ backend/src/controllers/noteController.js
@@ -0,0 +1,17 @@
+export function getAllNotes(req, res){
+    res.status(200).send("you just fetched the notes ");
+}
+
+export function createNote(req, res){
+    res.status(201).send("Note created successfully");
+}
+
+
+export function updateNote(req, res){
+    res.status(200).send("Note updated successfully");
+}
+
+export function deleteNote(req, res){
+    res.status(200).send("Note deleted successfully");
+}
+
--- backend/src/server.js
+++ backend/src/server.js
@@ -1,13 +1,13 @@
 import express from "express"
+import notesRoutes from "./routes/notesRoutes.js"
 
-const app = express()
 
-app.get("/api/notes", (req, res) => {
-    res.status(200).send("you got 10 notes ");
-});
+const app = express();
+
+
+app.use("/api/notes", notesRoutes);
 
 app.listen(5001, ()  => {
     console.log("server started on PORT: 5001");
 
 });
-
